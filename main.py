7190599c31dbd11716c10f60bc60f4732e38e5be
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 19:29:41 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# The map() function applies a given function 
+# to each item of an iterable (list, tuple etc.) and returns an iterator
+
+numbers = [2, 4, 6, 8, 10]
+
+# returns square of a number
+def square(number):
+  return number * number
+
+# apply square() function to each item of the numbers list
+squared_numbers_iterator = map(square, numbers)
+
+# converting to list
+squared_numbers = list(squared_numbers_iterator)
+print(squared_numbers)
+
+# Output: [4, 16, 36, 64, 100]
